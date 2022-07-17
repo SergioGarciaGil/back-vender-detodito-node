b444960e0e0b4453c4f6efd4e529b39d92c85c9e
SergioGarciaGil <sergio.cuerda@gmail.com>
2022-07-17 18:42:42 -0500

    configuracion passwortJWT

--- api/libs/auth.js
+++ api/libs/auth.js
@@ -2,9 +2,27 @@ const _ = require('underscore')
 const log = require('../../utils/logger')
 const usuarios = require('../../dataBase').usuarios
 const bcrypt = require('bcrypt')
-
-module.exports = (username, password, donne) => {// lo que hace es verificar si lo que esta llegando ala base de datos es corecta let 
-
+const passportJWT = require('passport-jwt')
 
 
+let jwtOptions = {
+    secetOrkey: 'esto es un secreto',
+    jwtFromRequest: passportJWT.ExtractJwt.fromAuthHeaderAsBearerToken()
 }
+
+module.exports = new passportJWT.Strategy(jwtOptions, (jwtPayload, next) => {
+    let index = _.findIndex(usuarios, usuario => usuario.id === jwtPayload.id)
+    if (index === -1) {
+        log.info(`JWT token no es walido. Usuario con id ${jwtPayload.id} no existe`)
+        next(null, false)
+        return;
+    } else {
+        //no hubo ningun error 
+        log.info(`Usiuario ${usuario[index].username} suministro un token valido. Autenticación completada`)
+        next(null, {// le clavamos un objeto nuevo si la utenticacion es exitosa
+            username: usuarios[index].username,
+            id: usuarios[index].id
+        })
+    }
+})
+
