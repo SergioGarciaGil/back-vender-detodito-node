45125d331ff3d33a38bc301320597b7b3757b89a
SergioGarciaGil <sergio.cuerda@gmail.com>
2022-08-10 10:59:18 -0400

    migracion BD auth-login-token

--- api/libs/auth.js
+++ api/libs/auth.js
@@ -1,10 +1,8 @@
 const _ = require('underscore')
 const log = require('../../utils/logger')
 const passportJWT = require('passport-jwt')
-
-const bcrypt = require('bcrypt')
-const usuarios = require('../../dataBase').usuarios
 const config = require('./../../config')
+const usuarioController = require('../recursos/usuarios/usuarios.controller')
 
 
 let jwtOptions = {
@@ -14,18 +12,23 @@ let jwtOptions = {
 
 module.exports = new passportJWT.Strategy(jwtOptions, (jwtPayload, next) => {
 
-    let index = _.findIndex(usuarios, usuario => usuario.id === jwtPayload.id)
-    if (index === -1) {
-        log.info(`JWT token no es walido. Usuario con id ${jwtPayload.id} no existe`)
-        next(null, false)
-
-    } else {
-        //no hubo ningun error 
-        log.info(`Usiuario ${usuarios[index].username} suministro un token valido. Autenticación completada`)
-        next(null, {// le clavamos un objeto nuevo si la utenticacion es exitosa
-            username: usuarios[index].username,
-            id: usuarios[index].id
+    usuarioController.obtenerUsuario({ id: jwtPayload.id })
+        .then(usuario => {
+            if (!usuario) {
+                log.info(`JWT token no es walido. Usuario con id ${jwtPayload.id} no existe`)
+                next(null, false)// aqui le decimos que fallo la validacion del token
+            }
+            log.info(`Usuario ${usuario.username} suministro un token valido. Autenticación completada`)
+            next(null, {
+                username: usuario.username,
+                id: usuario.id
+            })
         })
-    }
+
+        .catch(err => {
+            log.error("Error ocurrió al tratar de validar un token", err)
+            next(err)
+
+        })
 })
 
